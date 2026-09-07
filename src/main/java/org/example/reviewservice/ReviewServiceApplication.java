6049d1feeb586584c4d1c4abd566df7aed3cdffc
David Hasenson <davidhasenson@gmail.com>
2026-09-07 12:18:59 +0200

    updated how .env is loaded

--- src/main/java/org/example/reviewservice/ReviewServiceApplication.java
+++ src/main/java/org/example/reviewservice/ReviewServiceApplication.java
@@ -10,7 +10,11 @@ public class ReviewServiceApplication {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        dotenv.entries().forEach(entry -> {
+            if (System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
 
         SpringApplication.run(ReviewServiceApplication.class, args);
     }
